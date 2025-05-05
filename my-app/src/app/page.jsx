46111fb2c743cf4f639f16d1fd5618c1d070e062
Dionne <d.memie@gmail.com>
2025-05-05 16:41:36 +0100

    gap in iframe

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,34 +1,27 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './page.module.css'; // Import the CSS module
 
 export default function Home() {
   return (
     <div>
-      <span className='intro'>
-      Hi! I&apos;m Dionne. <br />
-      A problem solver with a passion for creating beautiful and functional applications. 
-
-      Take a look around to learn more about me and check out some of the work I have managed and developed!
+      <span className={styles.intro}>
+        Hi! I&apos;m Dionne. <br />
+        A problem solver with a passion for creating beautiful and functional applications.   
+        <br></br>
+        <br></br>
       </span>
-      
-      <div className='highlightsTitle'>
+   
+
+      <div className={styles.highlightsTitle}>
         <Link href="/projects">Project Highlights</Link>
       </div>
 
-      <div className="iframeContainer">
-        <iframe className="quizFrame" src="https://quiz-game-nine-vert.vercel.app/" height="500" width="320" title="QuizAcademy"></iframe>
-        <iframe src="https://fireplace-palace-six.vercel.app/" height="500" width="320" title="FireplacePalace"></iframe>
-        <iframe src="https://typescript-tutorial-self.vercel.app/" height="500" width="320" title="TypeScriptTut"></iframe>
+      <div className={styles.iframeContainer}>
+        <iframe className={styles.iframe} src="https://quiz-game-nine-vert.vercel.app/" height="500" width="320" title="QuizAcademy"></iframe>
+        <iframe className={styles.iframe} src="https://fireplace-palace-six.vercel.app/" height="500" width="320" title="FireplacePalace"></iframe>
+        <iframe className={styles.iframe} src="https://typescript-tutorial-self.vercel.app/" height="500" width="320" title="TypeScriptTut"></iframe>
       </div>
-
-      {/* <div className="projectContainer">
-        <span>
-          SATellite PREP
-          <div className='imageContainer'>
-            <Image src="/images/Satellite-romans.png" alt="image of website" width={300} height={400} />
-          </div>
-        </span>
-      </div> */}
     </div>
-  )
+  );
 }
